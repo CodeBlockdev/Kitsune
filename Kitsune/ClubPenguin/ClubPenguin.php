463d6d4a11b1cf22ff7f40a80db21de750a5c55c
Lucy <Lucy@Kitsune.org>
2014-05-11 17:51:00 -0700

    Added XML handlers
    As well as extension (world) handlers (an array, which is currently
    empty). The class is also now abstract.

--- Kitsune/ClubPenguin/ClubPenguin.php
+++ Kitsune/ClubPenguin/ClubPenguin.php
@@ -3,15 +3,35 @@
 namespace Kitsune\ClubPenguin;
 use Kitsune;
 
-class ClubPenguin extends Kitsune\Kitsune {
+abstract class ClubPenguin extends Kitsune\Kitsune {
 
+	// Maybe make these protected ?
 	private static $xml_handlers = array(
-		"policy" => "handlePolicyRequest"
+		"policy" => "handlePolicyRequest",
+		"verChk" => "handleVersionCheck",
+		"rndK" => "handleRandomKey", // A *request* for a random-key
+		"login" => "handleLogin"
+	);
+	
+	private static $world_handlers = array(
+		// Currently empty
 	);
 	
 	private function handlePolicyRequest($socket, $packet) {
 		$this->penguins[$socket]->send("<cross-domain-policy><allow-access-from domain='*' to-ports='*' /></cross-domain-policy>");
 	}
+	
+	private function handleVersionCheck($socket, $packet) {
+		$this->penguins[$socket]->send("<msg t='sys'><body action='apiOK' r='0'></body></msg>");
+	}
+	
+	private function handleRandomKey($socket, $packet) {
+		$penguin = $this->penguins[$socket];
+		$penguin->random_key = Hashing::generateRandomKey();
+		$penguin->send("<msg t='sys'><body action='rndK' r='-1'><k>" . $penguin->random_key . "</k></body></msg>");
+	}
+	
+	abstract protected function handleLogin($socket, $packet);
 	
 	protected function handleXmlPacket($socket, $packet) {
 		if(array_key_exists($packet::$handler, self::$xml_handlers)) {
@@ -23,7 +43,7 @@ class ClubPenguin extends Kitsune\Kitsune {
 	}
 	
 	protected function handleWorldPacket($socket, $packet) {
-		echo "$packet::$handler\n", print_r($packet);
+		echo "{$packet::$handler}\n", print_r($packet);
 	}
 	
 }
