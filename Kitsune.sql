2d8e7e7e2c6ea30833ad08c694f7384e623064b0
Lucy <Lucy@Kitsune.org>
2014-05-11 17:59:15 -0700

    File for database installation
    May drastically change over time.

---
+++ Kitsune.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 12, 2014 at 02:58 AM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `kitsune`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `igloos`
+--
+
+CREATE TABLE IF NOT EXISTS `igloos` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Owner` int(10) unsigned NOT NULL,
+  `Type` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `Floor` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `Music` smallint(6) NOT NULL DEFAULT '0',
+  `Furniture` text NOT NULL,
+  `Location` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `Likes` text NOT NULL,
+  `Locked` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `igloos`
+--
+
+INSERT INTO `igloos` (`ID`, `Owner`, `Type`, `Floor`, `Music`, `Furniture`, `Location`, `Likes`, `Locked`) VALUES
+(1, 101, 1, 0, 0, '', 0, '', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penguins`
+--
+
+CREATE TABLE IF NOT EXISTS `penguins` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Username` varchar(12) NOT NULL,
+  `Nickname` varchar(16) NOT NULL,
+  `Password` char(32) NOT NULL,
+  `ConfirmationHash` varchar(32) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  `SWID` char(38) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  `Email` char(254) NOT NULL,
+  `Inventory` text NOT NULL,
+  `Igloo` int(10) unsigned NOT NULL COMMENT 'Current active igloo',
+  `Igloos` text NOT NULL COMMENT 'Owned igloo types',
+  `Floors` text NOT NULL COMMENT 'Owned floorings',
+  `Locations` text NOT NULL COMMENT 'Owned locations',
+  `Furniture` text NOT NULL COMMENT 'Furniture inventory',
+  `Color` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `Head` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `Face` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `Neck` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `Body` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `Hand` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `Feet` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `Photo` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `Flag` smallint(5) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `Username` (`Username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=102 ;
+
+--
+-- Dumping data for table `penguins`
+--
+
+INSERT INTO `penguins` (`ID`, `Username`, `Nickname`, `Password`, `ConfirmationHash`, `SWID`, `Email`, `Inventory`, `Igloo`, `Igloos`, `Floors`, `Locations`, `Furniture`, `Color`, `Head`, `Face`, `Neck`, `Body`, `Hand`, `Feet`, `Photo`, `Flag`) VALUES
+(101, 'Arthur', 'Arthur', 'DC647EB65E6711E155375218212B3964', 'b15835f133ff2e27c7cb28117bfae8f4', '{de2da5a4-6d83-c05e-b774-0ab3773f5795}', 'lucy@kitsune.org', '', 1, '', '', '', '', 1, 0, 0, 0, 0, 0, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `puffles`
+--
+
+CREATE TABLE IF NOT EXISTS `puffles` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Name` char(12) NOT NULL,
+  `Hat` smallint(5) unsigned NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
