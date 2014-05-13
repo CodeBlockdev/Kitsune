b42ea0334ef7885575c5bc2bf2d564d84ae31e54
Lucy <Lucy@Kitsune.org>
2014-05-12 21:05:22 -0700

    Added a static variable $extension
    Used in determining which handler to call in the ClubPenguin class. Also
    added another array_shift for $xt_array.

--- Kitsune/ClubPenguin/Packets/Packet.php
+++ Kitsune/ClubPenguin/Packets/Packet.php
@@ -6,6 +6,7 @@ use Kitsune\ClubPenguin\Packets\Parsers;
 class Packet {
 
 	public static $is_xml;
+	public static $extension;
 	public static $handler;
 	public static $data;
 	
@@ -22,8 +23,11 @@ class Packet {
 			}
 		} else {
 			$xt_array = Parsers\XTParser::Parse($raw_data);
+			self::$extension = $xt_array[0];
 			self::$handler = $xt_array[1];
 			array_shift($xt_array);
+			array_shift($xt_array);
+			
 			self::$data = $xt_array;
 		}
 	}
