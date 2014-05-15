3975adfa175e001c7148835dbc0688ccbf3a6782
Lucy <Lucy@Kitsune.org>
2014-05-14 19:10:44 -0700

    Added loadPlayer, getPlayerString and more fields
    The loadPlayer method sets the new fields (clothing fields, age field,
    avatar field, coins field and inventory field). getPlayerString creates
    a string out of them.

--- Kitsune/ClubPenguin/Penguin.php
+++ Kitsune/ClubPenguin/Penguin.php
@@ -12,12 +12,65 @@ class Penguin {
 	public $identified;
 	public $random_key;
 	
+	public $color, $head, $face, $neck, $body, $hand, $feet, $photo, $flag;
+	public $age;
+
+	public $avatar;
+	public $coins;
+	public $inventory;
+	
+	public $x = 0;
+	public $y = 0;
+	public $frame;
+	
+	public $room;
+	
 	public $socket;
 	public $database;
 	
 	public function __construct($socket) {
 		$this->socket = $socket;
 		$this->database = new Kitsune\Database();
+	}
+	
+	public function loadPlayer() {
+		$this->random_key = null;
+		
+		$clothing = array("Color", "Head", "Face", "Neck", "Body", "Hand", "Feet", "Photo", "Flag");
+		$player = array("Avatar", "RegistrationDate", "Inventory", "Coins");
+		$columns = array_merge($clothing, $player);
+		$player_array = $this->database->getColumnsByName($this->username, $columns);
+		
+		list($this->color, $this->head, $this->face, $this->neck, $this->body, $this->hand, $this->feet, $this->photo, $this->flag) = array_values($player_array);
+		$this->age = floor((strtotime("NOW") - $player_array["RegistrationDate"]) / 86400); 
+		$this->avatar = $player_array["Avatar"];
+		$this->coins = $player_array["Coins"];
+		$this->inventory = explode('%', $player_array["Inventory"]);
+	}
+	
+	public function getPlayerString() {
+		$player = array(
+			$this->id,
+			$this->username,
+			45,
+			$this->color,
+			$this->head,
+			$this->face,
+			$this->neck,
+			$this->body,
+			$this->hand,
+			$this->feet,
+			$this->flag,
+			$this->photo,
+			$this->x,
+			$this->y,
+			$this->frame,
+			1,
+			146,
+			$this->avatar
+		);
+		
+		return implode('|', $player);
 	}
 	
 	public function send($data) {
