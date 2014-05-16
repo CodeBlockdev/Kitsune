1ca79f4da0b11b00ceb0f97475fb3c7b73889b58
Lucy <Lucy@Kitsune.org>
2014-05-15 20:13:42 -0700

    Room shuffling and clothing handlers
    Player joins a random (open) room. Handlers for clothing added. Buy
    inventory handler added (counts as clothing I guess). Actions are now
    handled. Clothing array added for determining item cost and lines with
    $packet::$data have been modified due to the removal of array_shift.

--- Kitsune/ClubPenguin/World.php
+++ Kitsune/ClubPenguin/World.php
@@ -16,11 +16,23 @@ final class World extends ClubPenguin {
 			"u#pbi" => "handleGetPlayerInfoById",
 			"u#sp" => "handleSendPlayerMove",
 			"u#sf" => "handleSendPlayerFrame",
-			"u#h" => "handleSendHeartbeat"
+			"u#h" => "handleSendHeartbeat",
+			"u#sa" => "handleUpdatePlayerAction",
+			"i#ai" => "handleBuyInventory",
+			"s#upc" => "handleSendUpdatePlayerClothing",
+			"s#uph" => "handleSendUpdatePlayerClothing",
+			"s#upf" => "handleSendUpdatePlayerClothing",
+			"s#upn" => "handleSendUpdatePlayerClothing",
+			"s#upb" => "handleSendUpdatePlayerClothing",
+			"s#upa" => "handleSendUpdatePlayerClothing",
+			"s#upe" => "handleSendUpdatePlayerClothing",
+			"s#upp" => "handleSendUpdatePlayerClothing",
+			"s#upl" => "handleSendUpdatePlayerClothing",
 		)
 	);
 	
 	private $rooms = array();
+	private $items = array();
 	
 	public function __construct() {
 		echo "Setting up rooms.. ";
@@ -30,6 +42,73 @@ final class World extends ClubPenguin {
 			$this->rooms[$room] = new Room($room, sizeof($this->rooms) + 1);
 		}
 		echo "done\n";
+		
+		echo "Building clothing list.. ";
+		$items_json = file_get_contents("http://media1.clubpenguin.com/play/en/web_service/game_configs/paper_items.json");
+		$items = json_decode($items_json, true);
+		foreach($items as $item) {
+			$item_id = $item["paper_item_id"];
+			$this->items[$item_id] = $item["cost"];
+			unset($items[$item_id]);
+		}
+		echo "done\n";
+	}
+	
+	// Because I'm super lazy
+	protected function handleSendUpdatePlayerClothing($socket, $packet) {
+		$penguin = $this->penguins[$socket];
+		$item_id = $packet::$data[2];
+		$clothing_type = substr($packet::$data[0], 2);
+		$clothing = array(
+			"upc" => "Color",
+			"uph" => "Head",
+			"upf" => "Face",
+			"upn" => "Neck",
+			"upb" => "Body",
+			"upa" => "Hand",
+			"upe" => "Feet",
+			"upp" => "Photo",
+			"upl" => "Flag"
+		);
+			
+		call_user_func(array($penguin, "update{$clothing[$clothing_type]}"), $item_id);
+	}
+	
+	protected function handleSendUpdatePlayerColour($socket, $packet) {
+		$item_id = $packet::$data[2];
+		
+		if(!is_numeric($item_id)) {
+			return;
+		} elseif(!in_array($item_id, $penguin->inventory)) {
+			return;
+		} else {
+			$penguin->updateColor($item_id);
+		}
+	}
+	
+	protected function handleBuyInventory($socket, $packet) {
+		$penguin = $this->penguins[$socket];
+		$item_id = $packet::$data[2];
+		
+		if(!isset($this->items[$item_id])) {
+			return $penguin->send("%xt%e%-1%402%");
+		} elseif(isset($this->penguin->inventory[$item_id])) {
+			return $penguin->send("%xt%e%-1%400%");
+		}
+		
+		$cost = $this->items[$item_id];
+		if($penguin->coins < $cost) {
+			return $penguin->send("%xt%e%-1%401%");
+		} else {
+			$penguin->addItem($item_id, $cost);
+		}
+	}
+	
+	protected function handleUpdatePlayerAction($socket, $packet) {
+		$penguin = $this->penguins[$socket];
+		$action_id = $packet::$data[2];
+		
+		$penguin->room->send("%xt%sa%{$penguin->room->internal_id}%{$penguin->id}%{$action_id}%");
 	}
 	
 	protected function handleSendHeartbeat($socket, $packet) {
@@ -41,32 +120,32 @@ final class World extends ClubPenguin {
 	protected function handleSendPlayerFrame($socket, $packet) {
 		$penguin = $this->penguins[$socket];
 		
-		$penguin->frame = $packet::$data[1];
+		$penguin->frame = $packet::$data[2];
 		$penguin->room->send("%xt%sf%{$penguin->room->internal_id}%{$penguin->id}%{$penguin->frame}%");
 	}
 		
 	protected function handleSendPlayerMove($socket, $packet) {
 		$penguin = $this->penguins[$socket];
 		
-		$penguin->x = $packet::$data[1];
-		$penguin->y = $packet::$data[2];
+		$penguin->x = $packet::$data[2];
+		$penguin->y = $packet::$data[3];
 		$penguin->room->send("%xt%sp%{$penguin->room->internal_id}%{$penguin->id}%{$penguin->x}%{$penguin->y}%"); 
 	}
 	
 	protected function handleGetPlayerInfoById($socket, $packet) {
 		$penguin = $this->penguins[$socket];
 		
-		if($penguin->database->playerIdExists($packet::$data[1])) {
-			$player_array = $penguin->database->getColumnsById($packet::$data[1], array("Username", "SWID"));
-			$penguin->send("%xt%pbi%{$penguin->room->internal_id}%{$player_array["SWID"]}%{$packet::$data[1]}%{$player_array["Username"]}%");
+		if($penguin->database->playerIdExists($packet::$data[2])) {
+			$player_array = $penguin->database->getColumnsById($packet::$data[2], array("Username", "SWID"));
+			$penguin->send("%xt%pbi%{$penguin->room->internal_id}%{$player_array["SWID"]}%{$packet::$data[2]}%{$player_array["Username"]}%");
 		}	
 	}
 	
 	protected function handleJoinRoom($socket, $packet) {
 		$penguin = $this->penguins[$socket];
 		
-		$room = $packet::$data[1];
-		$x = $packet::$data[2];
+		$room = $packet::$data[2];
+		$x = $packet::$data[3];
 		$y = $packet::$data[3];
 		
 		$this->joinRoom($penguin, $room, $x, $y);
@@ -106,14 +185,34 @@ final class World extends ClubPenguin {
 		$this->rooms[$room_id]->add($penguin);
 	}
 	
+	private function getOpenRoom() {
+		// Non-game/party rooms, perhaps have a totally separate array consisting of these room ids?
+		$room_ids = array_keys(
+			array_filter($this->rooms, function($room) {
+				if($room->external_id <= 810) {
+					return true;
+				}
+			})
+		);
+		
+		shuffle($room_ids);
+		foreach($room_ids as $room_id) {
+			if(sizeof($this->rooms[$room_id]->penguins) < 75) {
+				return $room_id;
+			}
+		}
+		
+		return 100;
+	}
+	
 	protected function handleJoinWorld($socket, $packet) {
 		$penguin = $this->penguins[$socket];
 		
-		if($penguin->id != $packet::$data[1]) {
+		if($penguin->id != $packet::$data[2]) {
 			return $this->removePenguin($penguin);
 		}
 		
-		$login_key = $packet::$data[2];
+		$login_key = $packet::$data[3];
 		$db_login_key = $penguin->database->getColumnById($penguin->id, "LoginKey");
 		
 		if($db_login_key != $login_key) {
@@ -135,7 +234,10 @@ final class World extends ClubPenguin {
 		
 		$load_player = "$player_string|%{$penguin->coins}%0%1440%$current_time%{$penguin->age}%0%7521%%7%1%0%211843";
 		$penguin->send("%xt%lp%-1%$load_player%");
-		$this->joinRoom($penguin, 100, 0, 0);
+		
+		$open_room = $this->getOpenRoom();
+		$this->joinRoom($penguin, $open_room, 0, 0);
+		
 		// The 0 after the player id is probably a transformation id, will be looking into a proper implementation
 		$penguin->room->send("%xt%spts%-1%{$penguin->id}%0%{$penguin->avatar}%");
 	}
