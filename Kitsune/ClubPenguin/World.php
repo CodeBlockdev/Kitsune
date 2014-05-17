a13fec1d261722ac8e4853e37f98a8d60697f8e7
Lucy <Lucy@Kitsune.org>
2014-05-17 12:48:20 -0700

    Added igloo handlers
    handleGetFurnitureInventory, handleGetActiveIgloo, handleGetGameData,
    handleGetPufflesByPlayerId, handleJoinPlayerRoo, handleBuyIglooLocation,
    handleGetAllIglooLayouts, and handleUpdateIglooConfiguration. Some of
    these handlers are still being worked on. Added $downloadAndDecode
    anonymous function for downloading decoding JSON in __construct, also
    added $locations for location purchasing.

--- Kitsune/ClubPenguin/World.php
+++ Kitsune/ClubPenguin/World.php
@@ -28,30 +28,156 @@ final class World extends ClubPenguin {
 			"s#upe" => "handleSendUpdatePlayerClothing",
 			"s#upp" => "handleSendUpdatePlayerClothing",
 			"s#upl" => "handleSendUpdatePlayerClothing",
+			"g#gii" => "handleGetFurnitureInventory",
+			"g#gm" => "handleGetActiveIgloo",
+			"g#ggd" => "handleGetGameData",
+			"p#pg" => "handleGetPufflesByPlayerId",
+			"j#jp" => "handleJoinPlayerRoom",
+			"g#aloc" => "handleBuyIglooLocation",
+			"g#gail" => "handleGetAllIglooLayouts",
+			"g#uic" => "handleUpdateIglooConfiguration"
 		)
 	);
 	
 	private $rooms = array();
 	private $items = array();
+	private $locations = array();
 	
 	public function __construct() {
+		$downloadAndDecode = function($url) {
+			$json_data = file_get_contents($url);
+			$data_array = json_decode($json_data, true);
+			return $data_array;
+		};
+		
 		echo "Setting up rooms.. ";
-		$rooms_json = file_get_contents("http://media1.clubpenguin.com/play/en/web_service/game_configs/rooms.json");
-		$rooms = json_decode($rooms_json, true);
+		$rooms = $downloadAndDecode("http://media1.clubpenguin.com/play/en/web_service/game_configs/rooms.json");
 		foreach($rooms as $room => $details) {
 			$this->rooms[$room] = new Room($room, sizeof($this->rooms) + 1);
+			unset($rooms[$room]);
 		}
 		echo "done\n";
 		
 		echo "Building clothing list.. ";
-		$items_json = file_get_contents("http://media1.clubpenguin.com/play/en/web_service/game_configs/paper_items.json");
-		$items = json_decode($items_json, true);
+		$items = $downloadAndDecode("http://media1.clubpenguin.com/play/en/web_service/game_configs/paper_items.json");
 		foreach($items as $item) {
 			$item_id = $item["paper_item_id"];
 			$this->items[$item_id] = $item["cost"];
 			unset($items[$item_id]);
 		}
 		echo "done\n";
+		
+		echo "Building location list.. ";
+		$locations = $downloadAndDecode("http://media1.clubpenguin.com/play/en/web_service/game_configs/igloo_locations.json");
+		foreach($locations as $location) {
+			$location_id = $location["igloo_location_id"];
+			$this->locations[$location_id] = $location["cost"];
+			unset($locations[$location_id]);
+		}
+		echo "done\n";
+	}
+	
+	protected function handleUpdateIglooConfiguration($socket, $packet) {
+		$penguin = $this->penguins[$socket];
+		
+		$player_id = $packet::$data[2];
+		$igloo_type = $packet::$data[3];
+		$floor = $packet::$data[4];
+		$location = $packet::$data[5];
+		$music = $packet::$data[6];
+		$furniture = $packet::$data[7];
+		
+		$penguin->database->updateIglooColumn($penguin->active_igloo, "Type", $igloo_type);
+		$penguin->database->updateIglooColumn($penguin->active_igloo, "Floor", $floor);
+		$penguin->database->updateIglooColumn($penguin->active_igloo, "Location", $location);
+		$penguin->database->updateIglooColumn($penguin->active_igloo, "Music", $music);
+		$penguin->database->updateIglooColumn($penguin->active_igloo, "Furniture", $furniture);
+		
+		$penguin->send("%xt%uic%{$penguin->room->internal_id}%$player_id%{$penguin->active_igloo}%$igloo_type:$floor:$location:$music:$furniture%");
+		
+		echo "HANDLE UPDATE IGLOO CONFIGURATION NOT DONE\n";
+	}
+	
+	protected function handleGetAllIglooLayouts($socket, $packet) {
+		$penguin = $this->penguins[$socket];
+		$player_id = $packet::$data[2];
+		
+		if($penguin->database->playerIdExists($player_id)) {
+			$igloo_layouts = $penguin->database->getAllIglooLayouts($player_id);
+			$active_igloo = $penguin->database->getColumnById($player_id, "Igloo");
+			
+			$penguin->send("%xt%gail%{$penguin->room->internal_id}%$active_igloo%$igloo_layouts%");
+		}
+	}
+	
+	protected function handleBuyIglooLocation($socket, $packet) {
+		$penguin = $this->penguins[$socket];
+		$location_id = $packet::$data[2];
+		
+		if(!isset($this->locations[$location_id])) {
+			return $penguin->send("%xt%e%-1%402%");
+		} elseif(isset($this->penguin->locations[$location_id])) {
+			return $penguin->send("%xt%e%-1%400%");
+		}
+		
+		$cost = $this->locations[$location_id];
+		if($penguin->coins < $cost) {
+			return $penguin->send("%xt%e%-1%401%");
+		} else {
+			$penguin->buyLocation($location_id, $cost);
+		}
+	}
+	
+	protected function handleJoinPlayerRoom($socket, $packet) {
+		$penguin = $this->penguins[$socket];
+		$player_id = $packet::$data[2];
+		
+		if($penguin->database->playerIdExists($player_id)) {
+			$external_id = $player_id + 1000;
+			
+			if(!isset($this->rooms[$external_id])) {
+				$this->rooms[$external_id] = new Room($external_id, $player_id);
+			}
+			
+			$penguin->send("%xt%jp%$player_id%$external_id%");
+			$this->joinRoom($penguin, $external_id);
+		}
+	}
+	
+	// TODO: Work on puffle adoption so this actually does something
+	protected function handleGetPufflesByPlayerId($socket, $packet) {
+		$penguin = $this->penguins[$socket];
+		$penguin->send("%xt%pg%{$penguin->room->internal_id}%%");
+	}
+	
+	protected function handleGetGameData($socket, $packet) {
+		$penguin = $this->penguins[$socket];
+		$penguin->send("%xt%ggd%{$penguin->room->internal_id}%Kitsune%");
+	}
+	
+	protected function handleGetActiveIgloo($socket, $packet) {
+		$penguin = $this->penguins[$socket];
+		$player_id = $packet::$data[2];
+		
+		if($penguin->database->playerIdExists($player_id)) {
+			$active_igloo = $penguin->database->getColumnById($player_id, "Igloo");
+			
+			if($player_id == $penguin->id) {
+				$penguin->active_igloo = $active_igloo;
+			}
+			
+			$igloo_details = $penguin->database->getIglooDetails($active_igloo);
+			$penguin->send("%xt%gm%{$penguin->room->internal_id}%$player_id%$igloo_details%");
+		}
+	}
+	
+	protected function handleGetFurnitureInventory($socket, $packet) {
+		$penguin = $this->penguins[$socket];
+		
+		$furniture_list = $penguin->database->getColumnsById($penguin->id, array("Furniture", "Floors", "Igloos", "Locations"));
+		$furniture_list = implode('%', $furniture_list);
+		
+		$penguin->send("%xt%gii%{$penguin->room->internal_id}%$furniture_list%");
 	}
 	
 	// Because I'm super lazy
@@ -137,6 +263,10 @@ final class World extends ClubPenguin {
 		$y = $packet::$data[3];
 		
 		$this->joinRoom($penguin, $room, $x, $y);
+	}
+	
+	protected function handleGetABTestData($socket, $packet) {
+		
 	}
 	
 	protected function handleGetMail($socket, $packet) {
